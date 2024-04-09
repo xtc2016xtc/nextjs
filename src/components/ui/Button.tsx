d1d9bff204017f9defc572845b8bf67ca4e9c86b
xtc2016xtc <728611827@qq.com>
2024-04-09 12:11:17 +0800

    首先导入了 `cva` 函数以及 React 的函数型组件类型。接着，定义了一个 `ButtonProps` 接口，用于表示按钮的属性类型。然后，我使用 `cva` 函数创建一个按钮样式变量 `buttonVariants`。最后，创建了一个名为 `Button` 的函数型组件，使用 `buttonVariants` 变量为按钮应用样式

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,18 @@
-import { cva } from "class-variance-authority";
-import { FC } from "react";
+import { cva } from "class-variance-authority"; // 导入 cva 功能
+import { FC } from "react"; // 导入 React 的函数型组件类型
 
-interface ButtonProps {}
-
-const buttonVariants = cva(
-  'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none'
-)
-
-const Button:FC<ButtonProps> = ({}) => {
-  return <div>Button</div>
+interface ButtonProps {
+  // 按钮的属性类型
 }
+
+// 定义按钮样式变量，使用 cva 函数创建
+const buttonVariants = cva(
+  'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none',
+);
+
+const Button: FC<ButtonProps> = ({}) => {
+  // 按钮组件
+  return <div className={buttonVariants()}>Button</div>;
+};
 
 export default Button;
